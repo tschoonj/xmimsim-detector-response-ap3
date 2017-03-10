10c15bff18b86a2a14fc7d1d75851f69a381dcb3
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-10 18:26:10 +0000

    AppVeyor support with S3 deployment (#1)
    * AppVeyor support: first commit
    
    * AppVeyor support: switch to master tarball

---
+++ appveyor-build-script.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+
+cd $APPVEYOR_BUILD_FOLDER
+autoreconf -fi
+export CPPFLAGS=-I/usr/local/include
+export CFLAGS="-Wno-deprecated -Wno-deprecated-declarations"
+export CXXFLAGS="-Wno-deprecated -Wno-deprecated-declarations"
+./configure
+make
+make check
+make distcheck
